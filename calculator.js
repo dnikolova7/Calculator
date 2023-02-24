c77d6763c011271c05e016a6437747a5d1e9f228
Daniela Nikolova <dnikolova.inbox@gmail.com>
2023-02-24 18:13:15 +0200

    Calculator application
    Finished calculator application with only one function (+).

--- calculator.js
+++ calculator.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const app = express();
 
+var bodyParser = require("body-parser");
+const { request } = require('express');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
-  res.send("I love Vasko!");
+  res.sendFile(__dirname + "/index.html");
 });
+
+app.post("/", (req, res) => {
+
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
+
+  var result = num1 + num2; 
+
+  res.send("The result of your calculation is " + result);
+})
 
 app.listen(3000, () => {
   console.log("I am in 3000 port.");
