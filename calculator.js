953f9c3b240e21da90a3d8295e4c7b546b2f7223
Daniela Nikolova <dnikolova.inbox@gmail.com>
2023-02-26 18:18:50 +0200

    bmi calculator
    BMI calculator

--- calculator.js
+++ calculator.js
@@ -14,7 +14,7 @@ app.post("/", (req, res) => {
 
   var num1 = Number(req.body.num1);
   var num2 = Number(req.body.num2);
-
+  var a = 2;
   var result = num1 + num2; 
 
   res.send("The result of your calculation is " + result);
@@ -23,3 +23,23 @@ app.post("/", (req, res) => {
 app.listen(3000, () => {
   console.log("I am in 3000 port.");
 });
+
+// BMI Calculator
+
+app.get("/bmiCalculator", (req, res)=>{
+  res.sendFile(__dirname + "/bmiCalculator.html");
+})
+
+app.post ("/bmiCalculator", (req, res)=>{
+
+  var weight = Number(req.body.weight);
+  var height = Number(req.body.height)/100;
+
+  var bmi = weight/(height*height);
+
+  res.send("Your BMI is " + bmi);
+
+});
+
+
+
